04896cba05407dc19d9d89e8b3f0681973fae07c
jonsundelof <jon.sundelof@student.kyh.se>
2021-10-12 10:26:52 +0200

    :tada: Finished POST route to create Ad.
    :tada: Finished POST route to create Ad.

--- server/routes/api/ads.js
+++ server/routes/api/ads.js
@@ -1,10 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route   GET api/posts
-// @desc    Test Route
-// @access  Public
+//              Models
+const Ad = require('../../models/Ad');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+////////////////////////////////////////
 
-router.get('/', (req, res) => res.send('Posts route'));
+// @route   POST api/posts
+// @desc    Create a ad
+// @access  Private
+
+router.get(
+  '/',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('desc', 'Description is required').not().isEmpty(),
+      check('price', 'Price is required').not().isEmpty(),
+      check('terms', 'Terms are required').not().isEmpty(),
+      check('value', 'Value is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newAd = new Ad({
+        category: req.body.category,
+        title: req.body.title,
+        desc: req.body.desc,
+        pic: req.body.pic,
+        pickup: req.body.pickup,
+        adress: req.body.adress,
+        delivery: req.body.delivery,
+        price: req.body.price,
+        terms: req.body.terms,
+        value: req.body.value,
+        timeperiod: req.body.timeperiod,
+        user: req.user.id,
+      });
+
+      const ad = await newAd.save();
+
+      res.json(ad);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
