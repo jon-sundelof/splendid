d413ea5f9dceaca6af06321c6b1f20216ddecd58
jonsundelof <jon.sundelof@student.kyh.se>
2021-10-14 13:58:04 +0200

    :tada: Started working on categories list
    :tada: Started working on categories list

---
+++ client/src/components/list/CategoriesList.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+
+import styles from './CategoriesList.module.scss';
+
+const CategoriesList = () => {
+  const categoriesList = [
+    'Fritid och Hobby',
+    'Verktyg',
+    'Fest och Party',
+    'Fordon',
+    'Elektronik',
+  ];
+
+  return (
+  
+    /*     <nav className={styles.list_container}>
+      <ul>
+        {categoriesList.map((item, i) => {
+          return (
+            <li key={i}>
+              <Link to={`/category/${item.trim().toLowerCase()}`}>{item}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav> */
+  );
+};
+
+export default CategoriesList;
