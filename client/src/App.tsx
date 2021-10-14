d40465ee4cb247de02fd75e4f94548a531ff0498
jonsundelof <jon.sundelof@student.kyh.se>
2021-10-14 12:43:21 +0200

    :lipstick: Changed theme colors to match my design in material UI
    :lipstick: Changes theme colors to match my design in material UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './styles/global.scss';
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
 
 /* auth pages */
 import Signin from './pages/Signin';
@@ -15,20 +16,69 @@ import Navbar from './components/navbar/Navbar';
 import DiscoverAds from './pages/DiscoverAds';
 /***************************************/
 
+/* declare module '@material-ui/core/styles' {
+  interface Theme {
+    status: {
+      danger: React.CSSProperties['color'];
+    };
+  }
+
+  interface Palette {
+    neutral: Palette['primary'];
+  }
+  interface PaletteOptions {
+    neutral: PaletteOptions['primary'];
+  }
+
+  interface PaletteColor {
+    darker?: string;
+  }
+  interface SimplePaletteColorOptions {
+    darker?: string;
+  }
+  interface ThemeOptions {
+    status: {
+      danger: React.CSSProperties['color'];
+    }
+  }
+} */
+
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#F88472',
+      darker: '#053e85',
+    },
+    secondary: {
+      main: '#0B9882',
+      darker: '#053e85',
+    },
+    neutral: {
+      main: '#4fff58',
+      contrastText: '#fff',
+    },
+  },
+});
+
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <main>
-          <Switch>
-            <Route exact path='/login' component={Signin} />
-            <Route exact path='/signup' component={Signup} />
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Navbar />
+          <main>
+            <Switch>
+              <Route exact path='/login' component={Signin} />
+              <Route exact path='/signup' component={Signup} />
 
-            <Route exact path='/' component={DiscoverAds} />
-          </Switch>
-        </main>
-      </Router>
+              <Route exact path='/' component={DiscoverAds} />
+            </Switch>
+          </main>
+        </Router>
+      </ThemeProvider>
     </>
   );
 }
