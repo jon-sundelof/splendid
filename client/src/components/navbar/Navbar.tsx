fb86ce2c74ad1347d5575e1055065a0abafc19fd
jonsundelof <jon.sundelof@student.kyh.se>
2021-10-14 10:21:27 +0200

    :mag: Created navbar-component and stylesheet
    :mag: Created navbar-component and stylesheet

---
+++ client/src/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Browse Ads</li>
+        <li>Publish Ad</li>
+        <li>Profile</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
