2476f5ec0147f961c49a262951c6510917116078
jonsundelof <jon.sundelof@student.kyh.se>
2021-10-14 10:16:34 +0200

    :building_construction: Set up react-router in app component
    :building_construction: Set up react-router in app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './styles/global.scss';
+
+/* auth pages */
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+/**********************************/
+
+/*  layout components */
+import Navbar from './components/navbar/Navbar';
+/***************************************/
+
+/* pages */
+import DiscoverAds from './pages/DiscoverAds';
+/***************************************/
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1>Splendid! :)</h1>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <main>
+          <Switch>
+            <Route exact path='/login' component={Signin} />
+            <Route exact path='/signup' component={Signup} />
+
+            <Route exact path='/' component={DiscoverAds} />
+          </Switch>
+        </main>
+      </Router>
+    </>
   );
 }
 
