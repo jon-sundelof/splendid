05e58be54aef98a8174b5c4ce17520d8de0c5a84
jonsundelof <jon.sundelof@student.kyh.se>
2021-10-21 18:35:00 +0200

    :sparkles: Added publishAdForm to publish page
    :sparkles: Added publishAdForm to publish page in a styled container from MUI

--- client/src/pages/PublishAd.tsx
+++ client/src/pages/PublishAd.tsx
@@ -1,7 +1,95 @@
-import React from 'react';
+import * as React from 'react';
+
+/* SCSS */
+import '../styles/publishad.scss';
+/* **************************** */
+
+/* Components import */
+import PublishAdForm from '../components/forms/PublishAdForm';
+
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+
+import Typography from '@mui/material/Typography';
+
+interface State {
+  dayPrice: string;
+  threeDayPrice: string;
+  weekPrice: string;
+  productValue: string;
+}
 
 const PublishAd = () => {
-  return <div></div>;
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
+
+  const steps = ['Product info', 'Payment details', 'Review your ad'];
+
+  function getStepContent(step: number) {
+    switch (step) {
+      case 0:
+        return <PublishAdForm />;
+      /*      case 1:
+        return <PaymentForm />;
+      case 2:
+        return <Review />; */
+      default:
+        throw new Error('Unknown step');
+    }
+  }
+
+  return (
+    <main className='main_container_publish'>
+      <Container component='main' maxWidth='sm' sx={{ mb: 4 }}>
+        <Paper
+          variant='outlined'
+          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+        >
+          <Typography component='h1' variant='h4' align='center'>
+            Publish Ad
+          </Typography>
+          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          <React.Fragment>
+            <React.Fragment>
+              {getStepContent(activeStep)}
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                {activeStep !== 0 && (
+                  <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                    Back
+                  </Button>
+                )}
+                <Button
+                  variant='contained'
+                  onClick={handleNext}
+                  sx={{ mt: 3, ml: 1 }}
+                >
+                  {activeStep === steps.length - 1 ? 'Place order' : 'Next'}
+                </Button>
+              </Box>
+            </React.Fragment>
+          </React.Fragment>
+        </Paper>
+      </Container>
+    </main>
+  );
 };
 
 export default PublishAd;
