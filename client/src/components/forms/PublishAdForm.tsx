44f5bb79b8a26464514b80fb2fedc9d77e8253ca
jonsundelof <jon.sundelof@student.kyh.se>
2021-10-22 15:12:24 +0200

    :wrench: Added all values to ts interface and to useState
    :wrench: Added all values to ts interface and to useState

--- client/src/components/forms/PublishAdForm.tsx
+++ client/src/components/forms/PublishAdForm.tsx
@@ -16,19 +16,37 @@ import FormControl from '@mui/material/FormControl';
 import FormGroup from '@mui/material/FormGroup';
 
 interface State {
+  category: string;
+  title: string;
+  description: string;
   dayPrice: string;
   threeDayPrice: string;
   weekPrice: string;
   productValue: string;
+  pickup: boolean;
+  addresses: [string];
+  delivery: boolean;
+  cancellation: string;
+  iAgree: boolean;
 }
 
 const PublishAdForm = () => {
+  // const [category, setCategory] = React.useState('');
   const [values, setValues] = React.useState<State>({
+    category: '',
+    title: '',
+    description: '',
     dayPrice: '',
     threeDayPrice: '',
     weekPrice: '',
     productValue: '',
+    pickup: false,
+    addresses: [''],
+    delivery: false,
+    cancellation: '',
+    iAgree: false,
   });
+
   const Input = styled('input')({
     display: 'none',
   });
@@ -36,53 +54,55 @@ const PublishAdForm = () => {
   const handleChange =
     (prop: keyof State) => (event: React.ChangeEvent<HTMLInputElement>) => {
       setValues({ ...values, [prop]: event.target.value });
+      console.log(values);
     };
+
+  const handleChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValues({
+      ...values,
+      [event.target.name]: event.target.checked,
+    });
+
+    console.log(values);
+  };
+
   return (
     <React.Fragment>
       <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <label>1. Pick a category</label>
-          <CategoriesList />
-        </Grid>
         <Grid item xs={12} sm={6}>
-          <label>3. Describe your product</label>
+          <label>1. Describe your product</label>
           <TextField
             required
             id='title'
+            value={values.title}
+            onChange={handleChange('title')}
             name='title'
             label='Title'
             fullWidth
             variant='standard'
           />
         </Grid>
+        <Grid item xs={4}>
+          {/* <label>1. Pick a category</label> */}
+          <CategoriesList values={values} setCategory={setValues} />
+        </Grid>
         <Grid item xs={12}>
           <TextField
             id='outlined-multiline-static'
             label='Description'
+            value={values.description}
+            onChange={handleChange('description')}
             multiline
             rows={4}
             fullWidth
             style={{ marginTop: '1em' }}
           />
         </Grid>
-        <Grid item xs={12}>
-          <label>4. Upload image</label>
-          <label htmlFor='icon-button-file'>
-            <Input accept='image/*' id='icon-button-file' type='file' />
-            <IconButton
-              color='primary'
-              aria-label='upload picture'
-              component='span'
-            >
-              <PhotoCamera />
-            </IconButton>
-          </label>
-        </Grid>
         <label
           style={{ width: '100%', padding: '2em 24px 0 24px' }}
           className='section_label'
         >
-          5. Set prices
+          3. Set prices
         </label>
         <Grid item xs={12} sm={6}>
           <FormControl fullWidth sx={{ m: 1 }}>
@@ -116,7 +136,7 @@ const PublishAdForm = () => {
             />
           </FormControl>
         </Grid>
-        <Grid item xs={12} sm={6}>
+        <Grid item xs={12} sm={12}>
           <FormControl fullWidth sx={{ m: 1 }}>
             <InputLabel htmlFor='outlined-adornment-amount'>Week</InputLabel>
             <OutlinedInput
@@ -132,45 +152,96 @@ const PublishAdForm = () => {
             />
           </FormControl>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={4}>
           <label>6. Alternatives for pickup</label>
           <FormGroup>
             <FormControlLabel
-              control={<Checkbox defaultChecked />}
+              name='pickup'
+              control={
+                <Checkbox
+                  value={values.delivery}
+                  onChange={handleChangeCheckbox}
+                />
+              }
               label='Pickup'
             />
             <FormControlLabel
-              control={<Checkbox defaultChecked />}
+              name='delivery'
+              control={
+                <Checkbox
+                  value={values.delivery}
+                  onChange={handleChangeCheckbox}
+                />
+              }
               label='Delivery'
             />
           </FormGroup>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={8}>
+          <section>
+            <h2 style={{ color: 'green' }}>Delivery</h2>
+            <p>
+              Sp<span style={{ color: 'green' }}>lend</span>id offers users an
+              environmentally friendly delivery option to make the loan more
+              accessible to those who do not have the right conditions.
+              <br />
+              <br />
+              See how we work by following the{' '}
+              <span style={{ color: 'green' }}>link</span>
+            </p>
+          </section>
+        </Grid>
+        <Grid item xs={6}>
+          <TextField id='standard-basic' label='Address +' variant='standard' />
+        </Grid>
+        <Grid item xs={6}>
+          <label>2. Upload image</label>
+          <label htmlFor='icon-button-file'>
+            <Input accept='image/*' id='icon-button-file' type='file' />
+            <IconButton
+              color='primary'
+              aria-label='upload picture'
+              component='span'
+            >
+              <PhotoCamera />
+            </IconButton>
+          </label>
+        </Grid>
+        <Grid item xs={6}>
           <FormControl fullWidth sx={{ m: 1 }}>
             <label>7. Cancellation policy</label>
-            <TextField id='standard-basic' label='Policy' variant='standard' />
+            <TextField
+              id='cancellation'
+              value={values.cancellation}
+              onChange={handleChange('cancellation')}
+              label='Policy'
+              variant='standard'
+            />
           </FormControl>
         </Grid>
-        <Grid item xs={12}>
-          <span className='section_label'>8. The value of the product</span>
+        <Grid item xs={6}>
           <FormControl fullWidth sx={{ m: 1 }}>
-            <InputLabel htmlFor='outlined-adornment-amount'>Amount</InputLabel>
-            <OutlinedInput
-              size='small'
-              id='outlined-adornment-amount'
+            <label>8. Product value</label>
+            <TextField
+              id='productValue'
               value={values.productValue}
               onChange={handleChange('productValue')}
-              startAdornment={
-                <InputAdornment position='start'>$</InputAdornment>
-              }
-              label='Amount'
+              label='Value $'
+              variant='standard'
               type='number'
             />
           </FormControl>
         </Grid>
         <Grid item xs={12} sm={6}>
           <FormControlLabel
-            control={<Checkbox color='primary' name='iAgree' value='yes' />}
+            name='iAgree'
+            control={
+              <Checkbox
+                value={values.delivery}
+                onChange={handleChangeCheckbox}
+                color='primary'
+              />
+            }
             label='I agree to the terms of service'
           />
         </Grid>
