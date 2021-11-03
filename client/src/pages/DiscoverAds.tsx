f45591654d06d7d5df7f9e6dfd9a355ad26fbe76
jonsundelof <jon.sundelof@student.kyh.se>
2021-11-03 09:42:25 +0100

    :construction_worker: Added Ads component to discover-page
    :construction_worker: Added Ads component to discover-page

--- client/src/pages/DiscoverAds.tsx
+++ client/src/pages/DiscoverAds.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import { useEffect } from 'react';
 import '../styles/discover.scss';
+import { getAds } from '../actions/ads';
 
+import Ads from '../components/ads/Ads';
 import CategoriesList from '../components/list/CategoriesList';
 
 const DiscoverAds = () => {
   return (
     <main className='discover_main_container'>
       <CategoriesList />
+      <section className='ads_container'></section>
+      <Ads />
     </main>
   );
 };
