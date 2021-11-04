92618e972c3989fccdde555725674e2a72d6c61b
Jon Sundelöf <70135228+jon-sundelof@users.noreply.github.com>
2021-11-04 11:27:00 +0100

    :wrench: Connected targeted ad reducer
    :wrench: Connected targeted ad reducer

--- client/src/reducers/index.tsx
+++ client/src/reducers/index.tsx
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import ads from './ads';
+import targetedAd from './targetedAd';
 
-export default combineReducers({ alert, auth, ads });
+export default combineReducers({ alert, auth, ads, targetedAd });
