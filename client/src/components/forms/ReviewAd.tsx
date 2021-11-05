7e2eae39ab98ae4d804528a72646bb861d5b2f0b
jonsundelof <jon.sundelof@student.kyh.se>
2021-11-05 09:46:03 +0100

    :construction: Started working on review AD comp, now able to publish ad from here
    :construction: Started working on review AD comp, now able to publish ad from here

--- client/src/components/forms/ReviewAd.tsx
+++ client/src/components/forms/ReviewAd.tsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import TargetedAd from '../ads/TargetedAd';
 
-const ReviewAd = () => {
-  return <div></div>;
+import { connect } from 'react-redux';
+import { postAd } from '../../actions/ads';
+
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+
+const ReviewAd = ({
+  handleNext,
+  handleBack,
+  activeStep,
+  steps,
+  postAd,
+  values,
+}: any) => {
+  const publishAd = () => {
+    postAd(values);
+  };
+
+  return (
+    <>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <div
+          style={{ width: '100vw', display: 'flex', justifyContent: 'center' }}
+        >
+          <TargetedAd />
+        </div>
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        {activeStep !== 0 && (
+          <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+            Back
+          </Button>
+        )}
+        <Button variant='contained' onClick={publishAd} sx={{ mt: 3, ml: 1 }}>
+          {/* {activeStep === steps.length - 1 ? 'Place order' : 'Next'} */}
+          Publish
+        </Button>
+      </Box>
+    </>
+  );
 };
 
-export default ReviewAd;
+const mapStateToProps = (state: any) => ({
+  values: state.targetedAd.ad,
+});
+
+export default connect(mapStateToProps, { postAd })(ReviewAd);
