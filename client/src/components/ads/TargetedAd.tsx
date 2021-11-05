689257553727354307e7db6bea8c3a5e8effd1f4
jonsundelof <jon.sundelof@student.kyh.se>
2021-11-05 09:44:24 +0100

    :wrench: Added conditional render for targetet ad to only render when loaded
    :wrench: Added conditional render for targetet ad to only render when loaded

--- client/src/components/ads/TargetedAd.tsx
+++ client/src/components/ads/TargetedAd.tsx
@@ -2,41 +2,42 @@ import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getTargetedAd } from '../../actions/ads';
+import targetedAd from '../../reducers/targetedAd';
 
-const TargetedAd = ({ getTargetedAd, ad, id }: any) => {
-  //   const { ad } = targetedAd;
+import styles from './TargetedAd.module.scss';
+
+const TargetedAd = ({ getTargetedAd, ad, loading, id }: any) => {
   useEffect(() => {
     getTargetedAd(id);
+    console.log(loading);
   }, [getTargetedAd]);
   return (
     <>
-      <div>{ad.title}</div>
-      <h1>{ad.desc}</h1>
-    </>
-    /*       <article
-        id={item._id}
-        className='ad_article'
-        key={i}
-        onClick={(e) => onClickAd(e)}
-      >
-        <div className='ad_top_row'>
-          <h2>{item.title}</h2>
-          <span>{item.category}</span>
-        </div>
+      {loading ? null : (
+        <>
+          <article className={styles.sp_ad_container}>
+            <div className='ad_top_row'>
+              <h2>{ad.title}</h2>
+              <span>{ad.category}</span>
+            </div>
 
-        <p>{item.desc}</p>
- 
-        <div className='ads_prices'>
-          <p>1 day: {item.price[0]} $</p>
-          <p>3 days: {item.price[0]} $</p>
-          <p>7 days: {item.price[2]} $</p>
-        </div>
-      </article> */
+            <p>{ad.desc}</p>
+
+            <div className='ads_prices'>
+              <p>1 day: {ad.price[0]} $</p>
+              <p>3 days: {ad.price[0]} $</p>
+              <p>7 days: {ad.price[2]} $</p>
+            </div>
+          </article>
+        </>
+      )}
+    </>
   );
 };
 
 const mapStateToProps = (state: any) => ({
   ad: state.targetedAd.ad,
+  loading: state.targetedAd.loading,
 });
 
 export default connect(mapStateToProps, { getTargetedAd })(TargetedAd);
